b9416938c4e08027edeabeec429a565def0e4ac4
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-06-28 23:55:45 +0000

    added streaming functionality

--- streaming/app/main.py
+++ streaming/app/main.py
@@ -1,19 +1,49 @@
 import time
 import os
 import subprocess
+import psycopg2
 
-path = os.getcwd()
+root = '/data'
+path = os.path.join(root, 'live/')
+
+sql_host = os.environ['SQL_HOST']
+sql_user = os.environ['SQL_USER']
+sql_pass = os.environ['SQL_PASS']
+sql_db = os.environ['SQL_DB']
+
+if not os.path.isdir(path):
+  os.mkdir(path)
 
 def make_cmd(url, path):
-  return ['ffmpeg', '-i', url, '-f', 'image2', '-vf', "scale=w='min(800\, iw*3/2):h=-1' ", \
+  return ['ffmpeg', '-rtsp_transport', 'tcp', '-i', url, '-f', 'image2', '-vf', "scale=w='min(800\, iw*3/2):h=-1' ", \
     '-r', '1', '-strftime', '1', '{}/%s.jpg'.format(path)]
 
 
 def run():
+  # Get gates from database
+  try:
+    conn = psycopg2.connect(host=sql_host, user=sql_user, password=sql_pass, dbname=sql_db)
+  except:
+    print("Unable to connect to the database")
+
+  c = conn.cursor()
+  c.execute("SELECT id, uri_rtsp FROM gate;")
+  result = c.fetchall()
+
+  gates = []
+
+  for g in result:
+    gates.append({'id': g[0], 'rtsp': g[1]})
+
+  c.close()
+  conn.close()
+  
+  """
   gates = [
     {'id': 1, 'rtsp': 'rtsp://admin:@192.168.1.102:554/h264Preview_01_main'},
     {'id': 2, 'rtsp': 'rtsp://admin:@192.168.1.104:554/h264Preview_01_main'}
   ]
+  """
 
   # First time setup
   for i,gate in enumerate(gates):
@@ -22,11 +52,12 @@ def run():
     if not os.path.isdir(gate['path']):
       os.mkdir(gate['path'])
 
-    gates[i]['process'] = subprocess.Popen(make_cmd(gate['rtsp'], gate['path']))
+    time.sleep(2)
+    gates[i]['process'] = subprocess.Popen(make_cmd(gate['rtsp'], gate['path']), stdout=subprocess.PIPE)
 
   # Infinite loop
   while True:
-    print("Working on something...")
+    print("Refreshing")
 
     for i,gate in enumerate(gates):
       if gate['process'].poll() is None:
@@ -36,6 +67,10 @@ def run():
         print('Stream for gate {} dead with exit code {}, restarting'.format(gate['id'], gate['process'].poll()))
         gates[i]['process'] = subprocess.Popen(make_cmd(gate['rtsp'], gate['path']))
 
+    # Delete all snapshots older than one minute
+
+    del_cmd = 'find %s -mmin +1 -type f -exec rm -fv {} \;' % (path)
+    subprocess.Popen(del_cmd, shell=True)
 
     time.sleep(5)
 
