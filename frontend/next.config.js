44110f48614ed861ac3838266bb2c23aac198f0c
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2022-03-07 16:13:16 +0100

    Move next-auth API route to /auth

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_MOCKING: process.env.NEXT_MOCKING
-  }
+  },
+  async rewrites() { return [
+    { source:"/auth/:path*", destination: "/api/auth/:path*" }
+  ]}
 };
 
 module.exports = nextConfig;
